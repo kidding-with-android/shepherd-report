8d49b5914395934c314fe352acffa37edb70c5bd
Rafael Neves <rafael@giver.com.br>
2019-08-17 11:09:33 -0300

    Member add #WIP

--- lib/member_list.dart
+++ lib/member_list.dart
@@ -6,27 +6,18 @@ class MemberList extends StatefulWidget {
 }
 
 class MemberListState extends State<MemberList> {
-  final List<String> _members = <String>[];
-  // final Set<DateTime> _saved = Set<DateTime>();
-  // final TextStyle _biggerFont = const TextStyle(fontSize: 18);
-	// final String _localeName = "pt_BR";
-	// DateFormat formatter;
+  final List<Member> _members = <Member>[];
 
   @override
   Widget build(BuildContext context) {
-		// initializeDateFormatting(_localeName);
-		// formatter = DateFormat(DateFormat.YEAR_MONTH, _localeName);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Membros'),
       ),
-      body: Text('Testando'),
+      body: _buildMemberList(),
 			floatingActionButton: FloatingActionButton(
 				onPressed: () {
-					// Considerar usar BottomSheet
-					// https://api.flutter.dev/flutter/material/BottomSheet-class.html
-					_neverSatisfied();
+					_memberInclude();
 				},
 				child: Icon(Icons.add),
 				backgroundColor: Colors.yellow,
@@ -34,19 +25,78 @@ class MemberListState extends State<MemberList> {
     );
   }
 
-	Future<void> _neverSatisfied() async {
-		return showDialog<void>(
+	Widget _buildMemberList() {
+    return ListView.builder(
+			padding: const EdgeInsets.all(16),
+			itemCount: _members.length,
+			itemBuilder: (BuildContext _context, int i) {
+				if (i.isOdd) {
+					return Divider();
+				}
+
+				final int index = i ~/ 2;
+				return _buildRow(_members[index]);
+			}
+		);
+  }
+
+	Widget _buildRow(Member member) {
+		return ListTile(
+			title: Text(member.name),
+		);
+	}
+
+	Future<Widget> _memberInclude() async {
+    final _formKey = GlobalKey<FormState>();
+    Member _member = Member();
+
+		return showDialog<Widget>(
 			context: context,
 			barrierDismissible: false, // user must tap button!
 			builder: (BuildContext context) {
 				return AlertDialog(
 					title: Text('Adicione um membro'),
 					content: SingleChildScrollView(
-						child: TextField(
-							decoration: InputDecoration(
-								labelText: 'Nome',
-							),
-						)
+						child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Não esqueça do nome";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                    onSaved: (value) {
+											setState(() {
+                      	_member.name = value;
+											});
+                    },
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value.isNotEmpty && int.parse(value) < 10) {
+                        return "Mande ele para o kinder";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Idade'
+                    ),
+                    keyboardType: TextInputType.number,
+                    onSaved: (value) {
+											setState(() {
+                      	_member.age = int.parse(value);
+											});
+                    },
+                  )
+                ],
+              ),
+            )
 					),
 					actions: <Widget>[
 						FlatButton(
@@ -58,7 +108,10 @@ class MemberListState extends State<MemberList> {
 						FlatButton(
 							child: Text('Adicionar'),
 							onPressed: () {
-								Navigator.of(context).pop();
+                if (_formKey.currentState.validate()) {
+                  _addMember(_member);
+								  Navigator.of(context).pop();
+                }
 							},
 						),
 					],
@@ -67,86 +120,12 @@ class MemberListState extends State<MemberList> {
 		);
 	}
 
-  // List<DateTime> _generateMonths() {
-	// 	DateTime now = DateTime.now();
-	// 	if (_months.length > 0) {
-	// 		final DateTime last = _months.last;
-	// 		now = DateTime(last.year, last.month - 1);
-	// 	}
+  _addMember(Member member) {
+    _members.add(member);
+  }
+}
 
-	// 	return List.generate(10, (index) {
-	// 		return DateTime(now.year, now.month - index);
-	// 	});
-  // }
-
-  // void _pushSaved() {
-  //   Navigator.of(context).push(
-  //     MaterialPageRoute<void>(
-  //       builder: (BuildContext context) {
-  //         final Iterable<ListTile> tiles = _saved.map((DateTime month) {
-  //           return ListTile(
-  //             title: Text(
-  //               formatter.format(month),
-  //               style: _biggerFont,
-  //             ),
-  //           );
-  //         });
-  //         final List<Widget> divided = ListTile.divideTiles(
-  //           context: context,
-  //           tiles: tiles,
-  //         ).toList();
-
-  //         return Scaffold(
-  //           appBar: AppBar(
-  //             title: Text('Saved Suggestions'),
-  //           ),
-  //           body: ListView(
-  //             children: divided,
-  //           ),
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
-
-  // Widget _buildMonths() {
-  //   return ListView.builder(
-	// 		padding: const EdgeInsets.all(16),
-	// 		itemBuilder: (BuildContext _context, int i) {
-	// 			if (i.isOdd) {
-	// 				return Divider();
-	// 			}
-
-	// 			final int index = i ~/ 2;
-	// 			if (index >= _months.length) {
-	// 				_months.addAll(_generateMonths());
-	// 			}
-	// 			return _buildRow(_months[index]);
-	// 		}
-	// 	);
-  // }
-
-  // Widget _buildRow(DateTime month) {
-  //   final bool alreadySaved = _saved.contains(month);
-
-  //   return ListTile(
-  //     title: Text(
-  //       formatter.format(month),
-  //       style: _biggerFont,
-  //     ),
-  //     trailing: Icon(
-  //       alreadySaved ? Icons.favorite : Icons.favorite_border,
-  //       color: alreadySaved ? Colors.red : null,
-  //     ),
-  //     onTap: () {
-  //       setState(() {
-  //         if (alreadySaved) {
-  //           _saved.remove(month);
-  //         } else {
-  //           _saved.add(month);
-  //         }
-  //       });
-  //     },
-  //   );
-  // }
+class Member {
+  String name = '';
+  int age;
 }
